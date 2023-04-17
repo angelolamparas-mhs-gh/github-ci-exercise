0512ca569414381ba6ce12379149b9a50c60f5ef
Angelo Lamparas <angelo.lamparas@bld.ai>
2023-04-17 15:39:29 +0800

    github-ci-exercise: gatsby app

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `Github CI Exercise Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
